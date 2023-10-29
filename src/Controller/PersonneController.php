a53549f39d4e6dd053e426238eac48a919fa2845
Tchantcho-tchoutto-isaac <tchantchoisaac1997@gmail.com>
2023-10-29 18:56:44 +0100

    détail personne

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -8,9 +8,48 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('personne')]
 class PersonneController extends AbstractController
 {
-    #[Route('/personne/add', name: 'personne.add')]
+    // afficher toutes les personnes 
+    #[Route('/', name:'personne.List')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $repository = $doctrine->getRepository(Personne::class); 
+    
+        $personnes = $repository->findAll();
+    
+        return $this->render('personne/index.html.twig', ['personnes' => $personnes]);
+    }
+    
+    #[Route('/alls', name:'personne.alls')]
+    public function indexAlls(ManagerRegistry $doctrine): Response
+    {
+        $repository = $doctrine->getRepository(Personne::class); 
+    
+        $personnes = $repository->findBy(['age'=>'30']);
+    
+        return $this->render('personne/index.html.twig', ['personnes' => $personnes]);
+    }
+
+
+
+    #[Route('/{id<\d+>}', name:'personne.detail')]
+   // detail d'une personne
+    public function detail(Personne $personne =null): Response
+    {
+        
+        if(!$personne){
+            $this->addFlash(type:'error',message:"la personne n'exsite pas ");
+            return $this->redirectToRoute('personne.List');
+        }
+        return $this->render('personne/detail.html.twig', ['personne' => $personne]);
+    
+      
+    }
+
+
+    #[Route('/add', name: 'personne.add')]
     public function addPersonne(ManagerRegistry $doctrine): Response
     {
         $entityManager=$doctrine->getManager();
--- src/DataFixtures/PersonneFixture.php
+++ src/DataFixtures/PersonneFixture.php
@@ -4,13 +4,23 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Personne;
+use Faker\Factory;
 
 class PersonneFixture extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create(locale: "fr_FR");
+
+        for($i=0 ;$i<100 ;$i++ ){
+            $personne= new Personne();
+            $personne->setFirstname($faker->firstName);
+            $personne-> setname($faker->name);
+            $personne->setAge($faker->numberBetween(18,65));
+
+            $manager->persist($personne);
+        }
 
         $manager->flush();
     }
